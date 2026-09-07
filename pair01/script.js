9af9d4d3e46972391b40dcf53f774b7c3f8932ad
TymurD <tymur2910@gmail.com>
2026-09-07 11:12:11 +0300

    initial commit + pair01

---
+++ pair01/script.js
@@ -0,0 +1,74 @@
+//42;
+//4.5 Number;
+
+// 'use Str';
+
+true;
+1;
+
+
+//false;
+//0;
+//NaN;
+//null;
+//undefined;
+
+// x = y + 10;
+
+// var a;
+// a = 10;
+// 
+// console.log(a)
+// var b = 20;
+// var c = 12; d = 12;
+// 
+// let aNew = '32';
+
+// a = 13;
+// var a;
+// console.log(a);
+
+// const a = 4;
+
+// let a = 15; b = 26;
+// console.log(1 + b)
+// console.log(1 - b);
+// console.log(1 * b);
+// console.log(1 ** b);
+// console.log(1 / b);
+// console.log(1 % b);
+// a = a += 1;
+// a++;
+// a--;
+// let a = 2; b;
+// b = a++;
+// b = ++a;
+// console.log(b);
+
+// let a = '4';
+// let b = 2;
+// 
+// console.log(a - b);
+// console.log(a + b);
+// console.log((a - 0) + b);
+// 
+// a = Number(a);
+// console.log(a + b);
+
+// let name = 'Ivan';
+// let age = 18;
+// 
+// console.log(`Hello ${name}! you are ${age} year/s old`)
+
+//console.info("hellow world");
+//console.log("hello world");
+//console.warn("hello world");
+//console.error("hello world");
+
+// alert("hello");
+// let name = prompt("What's your name?");
+// alert(name);
+
+// let a = Number(prompt('number1?'));
+// let b = +prompt('number1?');
+// console.log(a + b);
