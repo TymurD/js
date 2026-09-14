f46ed174fb98a321882ac8d0a3855254e34ce2e6
TymurD <tymur2910@gmail.com>
2026-09-14 11:16:50 +0300

    add pair2 materials

---
+++ pair02/script.js
@@ -0,0 +1,110 @@
+// if (statement){
+//   action;
+// }
+// else if (){
+//
+// }
+// true 
+// 1
+
+
+// false
+// 0
+// 0n
+// "" ''
+// null
+// undefined
+// Nan
+
+
+// let a = 10, b = "10";
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a != b);
+// console.log(a !== b);
+// > < >= <=
+
+
+// let a = +prompt("Enter a number");
+// let b = +prompt("Enter second number");
+// let c;
+//
+// if (a > b) {
+//   a = 'a > b'
+// } else if (a < b) {
+//   c = 'a < b'
+// }
+// else {
+//   c = 'a == b'
+// }
+// alert(c);
+
+// if (a > b) c = "a > b";
+// else if (a < b) c = "a < b";
+// else c = "a == b";
+
+// let course = prompt("What's the name of the course?"), title;
+// switch (course) {
+//   case "figma":
+//     title = "Figma";
+//     break;
+//
+//   case "WEB":
+//     title = "HTML + CSS";
+//     break;
+//
+//   case "Javascript":
+//     title = "Javascript programming basics";
+//     break;
+//
+//   default:
+//     title = "No course found";
+// }
+// alert(title)
+
+
+
+
+// ------------------------------------------
+// problem 1
+
+// let product = prompt("What are you buying?");
+// let amount = +prompt("How many are you buying?");
+// let price = +prompt("How much does one cost?");
+//
+// if (amount * price >= 5000) total = (amount * price) * .9;
+// else total = amount * price;
+// alert(total)
+
+
+// problem 2
+
+// let type = prompt("which service?"), result;
+//
+// switch (type) {
+//   case "courrier":
+//     result = 200;
+//     break;
+//
+//   case "mail":
+//     result = 100;
+//     break;
+//
+//   case "takeout":
+//     result = "free";
+//     break;
+//
+//   default:
+//     result = null;
+// }
+// alert(result);
+
+// ------------------------------------------
+
+// let a = true;
+// console.log(a);
+//
+// let a = 10, b = 12, c = 5;
+// if (a > b && a > c) {
+//   console.log(a);
+// } else if (b > a || a > c) { }
